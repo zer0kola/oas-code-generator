7710bc8580d495302fdcc2276790cdbeb1182f1c
zer0kola <zer0kola321@gmail.com>
2024-12-07 17:48:29 +0900

    chore: update dependencies and remove unused types; add Swagger parser and OpenAPI types

--- packages/api/cli/index.ts
+++ packages/api/cli/index.ts
@@ -1,8 +1,9 @@
-import { swaggerCommand } from './command/swagger';
+// import { typeGenerateCommand } from './command/type-generate';
 
 async function main() {
   try {
-    await swaggerCommand();
+    // await swaggerCommand();
+    // await typeGenerateCommand();
   } catch (error: any) {
     console.error('Error:', error.message);
     process.exit(1);
